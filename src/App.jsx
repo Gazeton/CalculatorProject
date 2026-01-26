f6992a38797c7fe99e662532ca2941561fd7a3ff
Gazeton <155329087+Gazeton@users.noreply.github.com>
2026-01-26 13:25:00 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,12 @@ import './App.css'
 import Calculator from "./components/Calculator";
 
 
-export default function App() {
+function App() {
   return (
     <div>
       <h1>Kalkulator</h1>
     </div>
-  )
+  );
 }
 
-
 export default App;
-
